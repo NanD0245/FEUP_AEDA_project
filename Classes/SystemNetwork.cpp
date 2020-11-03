23cf74c0f782c7cc6632a718a86ec36156fd2de9
Tomás Fidalgo <up201906743@fe.up.pt>
2020-11-03 13:12:48 +0000

    systemworks write finished, still need to test it

--- Classes/SystemNetwork.cpp
+++ Classes/SystemNetwork.cpp
@@ -20,22 +20,33 @@ void SystemNetwork::write(string file){
     ofstream f(file);
 
     for (int i = 0;i<highways->highways.size();i++){
-        f<< "Highway nr"<<i+1<<": ";
+        f<< "Highway nr"<<i+1<<": "<<endl;
         f << highways->highways[i]->getName() <<endl;
         t= highways->highways[i]->tolls;
         for(int j=0;j<t.size();j++){
             f<< "  Toll nr"<<j+1<<endl;
-            f<<"  "<<t[j]->getInfo();
+            f<<"  "<<t[j]->getInfo()<<endl;
             l=t[j]->lanes;
             for(int k=0;k<l.size();k++){
                 f<< "    Lane nr"<<l[k]->getLaneNumber()<<endl;
-                f<<l[k]->getInfo();
+                f<<"    "<<l[k]->getInfo()<<endl;
             }
         }
     }
+    f<<endl;
     for(int i=0;i<movements->movements.size();i++){
         f<<"Movement nr"<<i+1<<": "<<endl;
         f<<movements->movements[i]->getInfo();
+    }
+    f<<endl;
+    for(int i=0;i<vehicles->vehicles.size();i++){
+        f<<"Taxes:";
+        for (int j=0;j<4;j++){
+            f<<" "<<vehicles->taxes[j];
+        }
+        f<<endl;
+        f<<"Vehicle nr"<<i+1<<": "<<endl;
+        f<<vehicles->vehicles[i]->getInfo()<<endl;
     }
 
     f.close();
